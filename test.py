26f3859c004b2f5275ec0649c955fc9626e4cc87
Bram del Canho <bramdelcanho@gmail.com>
2025-05-28 15:06:50 +0200

    Refactor simulation tests to include JIT results and improve result comparisons

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from lib import data
 from lib.random import disable_randomness
 from base import simulation as base_simulation
 from original import simulation as original_simulation
+from jit import simulation as jit_simulation
 from tqdm import tqdm
 import numpy as np
 
@@ -18,18 +19,30 @@ freq = 1.0
 disable_randomness()
 
 for i, speed in tqdm(enumerate([1.0, 2.0, 10.0])):
-    T, Xs = base_simulation.simulate(W_list, D_list, dt, tf, k, speed, freq)
+    T_base, Xs_base = base_simulation.simulate(W_list, D_list, dt, tf, k, speed, freq)
     T_original, Xs_original = original_simulation.simulate(W, D, dt, tf, k, speed, freq)
+    T_jit, Xs_jit = jit_simulation.simulate(W_list, D_list, dt, tf, k, speed, freq)
 
     # Compare results
-    assert len(T) == len(T_original), f"Length mismatch for speed {speed}"
-    assert len(Xs) == len(Xs_original), f"Length mismatch for speed {speed}"
+    assert (
+        len(T_base) == len(T_original) == len(T_jit)
+    ), f"Length mismatch in T for speed {speed}"
+    assert (
+        len(Xs_base) == len(Xs_original) == len(Xs_jit)
+    ), f"Length mismatch in Xs for speed {speed}"
 
-    for x, x_original in tqdm(zip(Xs, Xs_original)):
-        assert len(x) == len(x_original), f"Length mismatch in Xs for speed {speed}"
-        for xi, xi_original in zip(x, x_original):
+    for x_base, x_original, x_jit in zip(Xs_base, Xs_original, Xs_jit):
+        assert (
+            len(x_base) == len(x_original) == len(x_jit)
+        ), f"Length mismatch in Xs for speed {speed}"
+
+        for xi_base, xi_original, xi_jit in zip(x_base, x_original, x_jit):
             assert np.allclose(
-                xi, xi_original, atol=1e-6
-            ), f"Values mismatch for speed {speed}"
+                xi_base, xi_original
+            ), f"Mismatch in Xs for speed {speed} at {xi_base} vs {xi_original}"
+            assert np.allclose(
+                xi_base, xi_jit
+            ), f"Mismatch in Xs for speed {speed} at {xi_base} vs {xi_jit}"
+
 
 print("All tests passed successfully!")
